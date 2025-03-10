62ff59512cca28a4a6a655aec4c29a06a2298e23
Katei <kateievans28@gmail.com>
2025-03-10 16:14:16 +0300

    control flow

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  return (distance <= 400) ? 'This one is on me!' :
+  (distance <= 2000) ? 'That will be twenty bucks.' :
+  (distance <= 2500) ? 'I will gladly take your thirty bucks.' :
+  'No can do.';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+   // Write your code here!
+   return city === "NYC" ? "Ok, sounds good." : "No go."
 }
+ 
 
-function switchOnCharmFromTip(){
+
+function switchOnCharmFromTip(tip) {
   // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
